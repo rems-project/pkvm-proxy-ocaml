a53a662fe573e0bb4f78e432902db65bf03ad441
dk505 <dk505@cam.ac.uk>
2024-02-03 16:51:19 +0000

    tweak the fault address

--- src-bin/pkvm_tests.ml
+++ src-bin/pkvm_tests.ml
@@ -38,6 +38,7 @@ let main xs =
  *)
 let rec vcpu_run_expect ?(exit = 2) ?esr ?far ?hpfar ?disr vcpu =
   let check a = function Some x -> x = a | _ -> true in
+  let pp_x lbl ppf = function Some a -> Fmt.pf ppf "%s 0x%Lx" lbl a | _ -> () in
   match Pkvm_proxy.vcpu_run vcpu with
   | 0 -> vcpu_run_expect ~exit ?esr ?far ?hpfar ?disr vcpu
   | res ->
@@ -46,9 +47,11 @@ let rec vcpu_run_expect ?(exit = 2) ?esr ?far ?hpfar ?disr vcpu =
       res = exit && check f.esr_el2 esr && check f.far_el2 far &&
       check f.hpfar_el2 hpfar && check f.disr_el1 disr in
     if not ok then (
-      Log.err (fun k -> k "vcpu run: exit %d, fault: %a" res pp_fault_info f);
-      invalid_arg "vcpu_run_expect"
-    )
+      Log.err (fun k ->
+        k "@[vcpu_run:@ exit %d (expect %d)@ fault %a@ expect %a, %a, %a, %a"
+        res exit pp_fault_info f
+        (pp_x "esr") esr (pp_x "far") far (pp_x "hpfar") hpfar (pp_x "disr") disr);
+      invalid_arg "vcpu_run_expect")
 
 let t_region_share () =
   let reg = kernel_region_alloc 0x1000 in
@@ -81,7 +84,7 @@ let t_vcpu_load_put () =
   teardown_vcpu vcpu
 
 let t_vcpu_run () =
-  let code = Pkvm_asm.(asm [mov (x 30) 0xdeadfffL; ldr (x 0) (x 30)]) in
+  let code = Pkvm_asm.(asm [mov (x 30) 0xdeadL; ldr (x 0) (x 30)]) in
   let vm = init_vm () in
   let vcpu = init_vcpu vm 0 in
   vcpu_load vcpu;
@@ -89,7 +92,7 @@ let t_vcpu_run () =
   kernel_region_release cbuf;
   Bigstring.blit_from_string code (region_memory cbuf);
   map_region_guest vcpu.mem cbuf 0x0L;
-  vcpu_run_expect vcpu ~esr:0x93c08006L ~hpfar:0xdead0L ~far:0xfffL;
+  vcpu_run_expect vcpu ~esr:0x93c08007L ~hpfar:0xd0L ~far:0xeadL;
   vcpu_put ();
   teardown_vm vm;
   teardown_vcpu vcpu;
@@ -99,9 +102,9 @@ let t_vcpu_run () =
 let t_guest_hvc_version () =
   let code = Pkvm_asm.(asm [
     hvc 0 VERSION_FUNC_ID;
-    mov (x 30) 0xdeadfffL;
-    ldr (x 0) (x 30)]
-  ) in
+    mov (x 30) 0xdeadL;
+    ldr (x 0) (x 30)
+  ]) in
   let vm = init_vm () in
   let vcpu = init_vcpu vm 0 in
   vcpu_load vcpu;
@@ -109,7 +112,7 @@ let t_guest_hvc_version () =
   kernel_region_release cbuf;
   Bigstring.blit_from_string code (region_memory cbuf);
   map_region_guest vcpu.mem cbuf 0x0L;
-  vcpu_run_expect vcpu ~esr:0x93c08006L ~hpfar:0xdead0L ~far:0xfffL;
+  vcpu_run_expect vcpu ~esr:0x93c08007L ~hpfar:0xd0L ~far:0xeadL;
   vcpu_put ();
   teardown_vm vm;
   teardown_vcpu vcpu;
@@ -119,9 +122,9 @@ let t_guest_hvc_version () =
 let t_guest_hvc_mem_share () =
   let code = Pkvm_asm.(asm [
     hvc 0 (VENDOR_HYP_KVM_MEM_SHARE_FUNC_ID (0x2000L, 0L, 0L));
-    mov (x 30) 0xdeadfffL;
-    ldr (x 0) (x 30)]
-  ) in
+    mov (x 30) 0xdeadL;
+    ldr (x 0) (x 30)
+  ]) in
   let vm = init_vm () in
   let vcpu = init_vcpu vm 0 in
   vcpu_load vcpu;
@@ -133,7 +136,7 @@ let t_guest_hvc_mem_share () =
   kernel_region_release mbuf;
   map_region_guest vcpu.mem mbuf 0x2000L;
   vcpu_run_expect vcpu ~esr:0x5a000000L ~hpfar:0x0L ~far:0x0L;
-  vcpu_run_expect vcpu ~esr:0x93c08006L ~hpfar:0xdead0L ~far:0xfffL;
+  vcpu_run_expect vcpu ~esr:0x93c08007L ~hpfar:0xd0L ~far:0xeadL;
   vcpu_put ();
   teardown_vm vm;
   teardown_vcpu vcpu;
@@ -146,9 +149,9 @@ let t_guest_hvc_mem_unshare () =
   let code = Pkvm_asm.(asm [
     hvc 0 (VENDOR_HYP_KVM_MEM_SHARE_FUNC_ID (0x2000L, 0L, 0L));
     hvc 0 (VENDOR_HYP_KVM_MEM_UNSHARE_FUNC_ID (0x2000L, 0L, 0L));
-    mov (x 30) 0xdeadfffL;
-    ldr (x 0) (x 30)]
-  ) in
+    mov (x 30) 0xdeadL;
+    ldr (x 0) (x 30)
+  ]) in
   let vm = init_vm () in
   let vcpu = init_vcpu vm 0 in
   vcpu_load vcpu;
@@ -161,7 +164,7 @@ let t_guest_hvc_mem_unshare () =
   map_region_guest vcpu.mem mbuf 0x2000L;
   vcpu_run_expect vcpu ~esr:0x5a000000L ~hpfar:0x0L ~far:0x0L;
   vcpu_run_expect vcpu ~esr:0x5a000000L ~hpfar:0x0L ~far:0x0L;
-  vcpu_run_expect vcpu ~esr:0x93c08006L ~hpfar:0xdead0L ~far:0xfffL;
+  vcpu_run_expect vcpu ~esr:0x93c08007L ~hpfar:0xd0L ~far:0xeadL;
   vcpu_put ();
   teardown_vm vm;
   teardown_vcpu vcpu;
