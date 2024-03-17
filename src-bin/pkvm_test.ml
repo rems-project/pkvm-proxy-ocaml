2ddee6cdd4e8caeb82ba75e5b49be54331e7c281
dk505 <dk505@cam.ac.uk>
2024-03-17 19:32:53 +0100

    (tests) basic parallel tests

--- src-bin/pkvm_test.ml
+++ src-bin/pkvm_test.ml
@@ -207,6 +207,42 @@ let t_guest_hvc_mem_unshare () =
   kernel_region_reclaim mbuf;
   kernel_region_free mbuf
 
+let t_vcpu_load_put_par_1 () =
+  let cpus = 4 in
+  let vm = init_vm ~vcpus:cpus () in
+  let vcpus = Array.init cpus (init_vcpu vm) in
+  let _ = spawnv ~cpus @@ fun i ->
+    for _ = 1 to 10 do
+      vcpu_load vcpus.(i);
+      vcpu_put ()
+    done
+  in
+  teardown_vm vm;
+  Array.iter teardown_vcpu vcpus
+
+(* Dance around initialising VCPUs on separate threads, which must be done in
+   order. This is only for testing — much saner to init them ahead of time on the
+   main thread. *)
+let t_vcpu_load_put_par_2 () =
+  let cpus = 4 in
+  let vm = init_vm ~vcpus:cpus () in
+  let sem = Array.init cpus @@ fun _ -> Semaphore.Binary.make false in
+  let vcpus = Array.init cpus @@ fun _ -> None in
+  let threads = List.init cpus @@ fun cpu ->
+    spawn ~cpu @@ fun () ->
+      Semaphore.Binary.acquire sem.(cpu);
+      let vcpu = init_vcpu vm cpu in
+      Semaphore.Binary.release sem.((cpu + 1) mod cpus);
+      vcpus.(cpu) <- Some vcpu;
+      for _ = 1 to 10 do
+        vcpu_load vcpu;
+        vcpu_put ()
+      done in
+  Semaphore.Binary.release sem.(0);
+  List.iter join threads;
+  teardown_vm vm;
+  Array.iter (function (Some vcpu) -> teardown_vcpu vcpu | _ -> assert false) vcpus
+
 let _ = main [
   "kernel share", t_region_share
 ; "kernel share+unshare", t_region_share_unshare
@@ -220,4 +256,6 @@ let _ = main [
 ; "guest hvc version", t_guest_hvc_version
 ; "guest hvc mem_share", t_guest_hvc_mem_share
 ; "guest hvc mem_unshare", t_guest_hvc_mem_unshare
+; "vcpu load+put parallel 1", t_vcpu_load_put_par_1
+; "vcpu load+put parallel 2", t_vcpu_load_put_par_2
 ]
