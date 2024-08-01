866a2b6bc2ef9ed66d5c5e7f48e97e9fd997a945
dk505 <dk505@cam.ac.uk>
2024-08-01 17:18:59 +0200

    (lib) split out (private) types module

--- src/pkvm_proxy.ml
+++ src/pkvm_proxy.ml
@@ -1,4 +1,5 @@
 open Bigarray
+include Pkvm_types
 open Pkvm_proxy_utils
 
 module Log = (val Logs.(Src.create "Pkvm_proxy" |> src_log))
@@ -14,43 +15,6 @@ let pkvm =
       Log.err (fun k -> k "Cannot open pkvm proxy — we need to run as root!");
       exit 1
   | fd -> reset_early_log log_cfg; fd
-
-(** Hyp-proxy types **)
-
-(** Host hypercalls. `enum __kvm_host_smccc_func`, and their ioctl arguments. **)
-type _ host_smccc_func =
-
-(* Hypercalls available only prior to pKVM finalisation *)
-  | Kvm_hyp_init
-  | Kvm_get_mdcr_el2
-  | Pkvm_init
-  | Pkvm_create_private_mapping
-  | Pkvm_cpu_set_vector
-  | Kvm_enable_ssbs
-  | Vgic_v3_init_lrs
-  | Vgic_v3_get_gic_config
-  | Kvm_flush_vm_context
-  | Kvm_tlb_flush_vmid_ipa
-  | Kvm_tlb_flush_vmid
-  | Kvm_flush_cpu_context
-  | Pkvm_prot_finalize
-
-(* Hypercalls available after pKVM finalisation *)
-  | Pkvm_host_share_hyp    : int64 -> unit host_smccc_func (* highly unsafe *)
-  | Pkvm_host_unshare_hyp  : int64 -> unit host_smccc_func
-  | Pkvm_host_reclaim_page : int64 -> unit host_smccc_func
-  | Pkvm_host_map_guest    : int64 * int64 -> unit host_smccc_func
-  | Kvm_adjust_pc
-  | Kvm_vcpu_run           : int64 -> int host_smccc_func
-  | Kvm_timer_set_cntvoff
-  | Vgic_v3_save_vmcr_aprs
-  | Vgic_v3_restore_vmcr_aprs
-  | Pkvm_init_vm           : int64 * int64 * int64 * int64 -> int host_smccc_func
-  | Pkvm_init_vcpu         : int * int64 * int64 -> unit host_smccc_func
-  | Pkvm_teardown_vm       : int -> unit host_smccc_func
-  | Pkvm_vcpu_load         : int * int * int64 -> unit host_smccc_func
-  | Pkvm_vcpu_put          : unit host_smccc_func
-  | Pkvm_vcpu_sync_state   : unit host_smccc_func
 
 (** FFI **)
 
---
+++ src/pkvm_types.ml
@@ -0,0 +1,36 @@
+(** Host hypercalls. `enum __kvm_host_smccc_func`, and their ioctl arguments. **)
+type _ host_smccc_func =
+
+(* Hypercalls available only prior to pKVM finalisation *)
+  | Kvm_hyp_init
+  | Kvm_get_mdcr_el2
+  | Pkvm_init
+  | Pkvm_create_private_mapping
+  | Pkvm_cpu_set_vector
+  | Kvm_enable_ssbs
+  | Vgic_v3_init_lrs
+  | Vgic_v3_get_gic_config
+  | Kvm_flush_vm_context
+  | Kvm_tlb_flush_vmid_ipa
+  | Kvm_tlb_flush_vmid
+  | Kvm_flush_cpu_context
+  | Pkvm_prot_finalize
+
+(* Hypercalls available after pKVM finalisation *)
+  | Pkvm_host_share_hyp    : int64 -> unit host_smccc_func (* highly unsafe *)
+  | Pkvm_host_unshare_hyp  : int64 -> unit host_smccc_func
+  | Pkvm_host_reclaim_page : int64 -> unit host_smccc_func
+  | Pkvm_host_map_guest    : int64 * int64 -> unit host_smccc_func
+  | Kvm_adjust_pc
+  | Kvm_vcpu_run           : int64 -> int host_smccc_func
+  | Kvm_timer_set_cntvoff
+  | Vgic_v3_save_vmcr_aprs
+  | Vgic_v3_restore_vmcr_aprs
+  | Pkvm_init_vm           : int64 * int64 * int64 * int64 -> int host_smccc_func
+  | Pkvm_init_vcpu         : int * int64 * int64 -> unit host_smccc_func
+  | Pkvm_teardown_vm       : int -> unit host_smccc_func
+  | Pkvm_vcpu_load         : int * int * int64 -> unit host_smccc_func
+  | Pkvm_vcpu_put          : unit host_smccc_func
+  | Pkvm_vcpu_sync_state   : unit host_smccc_func
+
+
